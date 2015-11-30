dd438caf96297097cbd61788ef963ebe3d76dd72
houke <houke.cn@gmail.com>
2015-11-30 12:20:00 +0800

    My scipts to change Ubuntu wall paper.

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
 
-PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
+PICTURE_DIR="$HOME/Pictures/bing_wallpapers/"
 
 mkdir -p $PICTURE_DIR
 
-urls=( $(curl -s http://cn.bing.com | \
-    grep -Eo "g_img={url:'.*?'" | \
-    sed -e "s/g_img={url:'\([^']*\)'.*/\1/" | \
-    sed -e "s/\\\//g") )
+urls=( $(curl -L http://cn.bing.com | \
+  grep -Eo "g_img={url:'.*?'" | \
+  sed -e "s/g_img={url:'\([^']*\)'.*/\1/" | \
+  sed -e "s/\\\//g") )
 
 for p in ${urls[@]}; do
-    echo $p
-    filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
-    if [ ! -f $PICTURE_DIR/$filename ]; then
-        echo "Downloading: $filename ..."
-        curl -Lo "$PICTURE_DIR/$filename" $p
-    else
-        echo "Skipping: $filename ..."
-    fi
+  wallpaper_location=$p
+  if [[ $p != *"cn.bing"* ]] 
+  then
+    bing_prefix="cn.bing.com"
+    wallpaper_location=$bing_prefix$p
+  fi
+  echo $wallpaper_location
+  filename=$(echo $wallpaper_location|sed -e "s/.*\/\(.*\)/\1/")
+  if [ ! -f $PICTURE_DIR/$filename ]; then
+      echo "Downloading: $filename ..."
+      curl -Lo "$PICTURE_DIR/$filename" $wallpaper_location
+  else
+      echo "Skipping: $filename ..."
+  fi
 done
+
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
+sleep 5
+gsettings set org.gnome.desktop.background picture-uri file://$PICTURE_DIR/$filename
+
